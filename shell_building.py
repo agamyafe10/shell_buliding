9ecd5b33e3802cfa1f3083d2fa0f7a371b2ce8b6
agamyafe10 <71751253+agamyafe10@users.noreply.github.com>
2021-10-31 10:45:26 +0200

    CLASS PROGRESS

--- shell_building.py
+++ shell_building.py
@@ -2,9 +2,39 @@ import os.path
 from shell_help_func import *
 
 
+curr_dir = ""# get the current directory
+get_curr_dir()
 file_path = ""
-client = ""# gets first input from the uder to start the while
-while client != "exit":
-    client = input("ENTER YOUR COMMAND")
-    print("the command you entered is: ", client)
-    is_command_file(client)# going through all the files looking if on's name identical to the command entered
+cmd = ""# gets first input from the uder to start the while
+while cmd!= "exit":\
+    #  going through all the files looking if on's name identical to the command entered
+    cmd = input("ENTER YOUR COMMAND: ")
+    print("the command you entered is: ", cmd)
+    is_command_file(cmd)
+    # GOING THROUGH ALL THE CMD OPTIONS
+    if cmd == "dir":
+        print_dir(curr_dir)
+    elif cmd == "cd":
+        change_dir()
+    elif cmd == "md":
+        make_dir()        
+    elif cmd == "echo":
+        pass
+    elif cmd == "hello.py":
+        pass
+    elif cmd == "Hex_dump":
+        pass
+    elif cmd == "pipe":
+        pass
+    elif cmd == "Redirection":
+        pass
+    elif cmd == "find":
+        pass
+    
+    
+def get_curr_dir():
+    "change the global variable to the currents dir and return the current directory as a string"
+    global curr_dir
+    curr_dir = os.getcwd()# get the current directory
+    print("the current directory is" + curr_dir)
+    return curr_dir
