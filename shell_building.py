2304d0018ad271ed83a1b451a3c66b13a0510487
agamyafe10 <71751253+agamyafe10@users.noreply.github.com>
2021-11-08 09:43:36 +0200

    class progress

--- shell_building.py
+++ shell_building.py
@@ -9,7 +9,29 @@ def get_curr_dir():
     print("the current directory is" + curr_dir)
     return curr_dir
 
-
+def func_switcher(func, cmd_det):
+    is_command_file(cmd)
+    # GOING THROUGH ALL THE CMD OPTIONS
+    if cmd == "dir":
+        get_curr_dir()
+        print_dir(curr_dir)
+    elif cmd == "cd":
+        change_dir(cmd_det[1])
+        get_curr_dir()
+    elif cmd == "md":
+        make_dir(cmd_det[1], cmd_det[2])        
+    elif cmd == "echo":
+        echo(cmd_det[1:])
+    elif cmd == "hello.py":
+        hello_py()
+    elif cmd == "Hex_dump":
+        HexDmp(cmd_det[1])
+    elif cmd == "find":
+        print("i am here bitches")
+        find(cmd_det[1].strip('"'), cmd_det[2])
+    
+    
+""" main code """
 curr_dir = ""# get the current directory
 get_curr_dir()
 file_path = ""
@@ -17,30 +39,17 @@ cmd = ""# gets first input from the uder to start the while
 while cmd!= "exit":
     #  going through all the files looking if on's name identical to the command entered
     command = input("ENTER YOUR COMMAND: ")
-    command_split = command.split(" ")
-    cmd = command_split[0]
-    print("the command you entered is: ", cmd)
-    is_command_file(cmd)
-    # GOING THROUGH ALL THE CMD OPTIONS
-    if cmd == "dir":
-        get_curr_dir()
-        print_dir(curr_dir)
-    elif cmd == "cd":
-        change_dir(command_split[1])
-        get_curr_dir()
-    elif cmd == "md":
-        make_dir(command_split[1], command_split[2])        
-    elif cmd == "echo":
-        echo(command_split[1:])
-    elif cmd == "hello.py":
-        hello_py()
-    elif cmd == "Hex_dump":
-        HexDmp(command_split[1])
-    elif cmd == "pipe":
-        pass
-    elif cmd == "Redirection":
-        pass
-    elif cmd == "find":
-        pass
-    
-    
+    if '|' in command:
+        # devides the input to the basic function
+        command_split = command.split("|")
+        first_command = command[0].split(" ")
+        second_command = command[1].split(" ")
+        pipe(first_command, second_command)
+    else:   
+        command_split = command.split(" ")
+        cmd = command_split[0]
+        print("the command you entered is: ", cmd)
+        func_switcher(cmd, command_split)
+
+
+
